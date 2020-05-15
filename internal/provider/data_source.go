16b18027309ee4364177c78eaf79aea965a620d5
Paul Tyng <paul@paultyng.net>
2020-05-15 09:27:08 -0400

    Add descriptions

--- internal/provider/data_source.go
+++ internal/provider/data_source.go
@@ -12,10 +12,29 @@ import (
 
 func dataSource() *schema.Resource {
 	return &schema.Resource{
+		Description: "The `external` data source allows an external program implementing a specific protocol " +
+			"(defined below) to act as a data source, exposing arbitrary data for use elsewhere in the Terraform " +
+			"configuration.\n" +
+			"\n" +
+			"**Warning** This mechanism is provided as an \"escape hatch\" for exceptional situations where a " +
+			"first-class Terraform provider is not more appropriate. Its capabilities are limited in comparison " +
+			"to a true data source, and implementing a data source via an external program is likely to hurt the " +
+			"portability of your Terraform configuration by creating dependencies on external programs and " +
+			"libraries that may not be available (or may need to be used differently) on different operating " +
+			"systems.\n" +
+			"\n" +
+			"**Warning** Terraform Enterprise does not guarantee availability of any particular language runtimes " +
+			"or external programs beyond standard shell utilities, so it is not recommended to use this data source " +
+			"within configurations that are applied within Terraform Enterprise.",
+
 		Read: dataSourceRead,
 
 		Schema: map[string]*schema.Schema{
 			"program": {
+				Description: "A list of strings, whose first element is the program to run and whose " +
+					"subsequent elements are optional command line arguments to the program. Terraform does " +
+					"not execute the program through a shell, so it is not necessary to escape shell " +
+					"metacharacters nor add quotes around arguments containing spaces.",
 				Type:     schema.TypeList,
 				Required: true,
 				Elem: &schema.Schema{
@@ -24,12 +43,16 @@ func dataSource() *schema.Resource {
 			},
 
 			"working_dir": {
+				Description: "Working directory of the program. If not supplied, the program will run " +
+					"in the current directory.",
 				Type:     schema.TypeString,
 				Optional: true,
 				Default:  "",
 			},
 
 			"query": {
+				Description: "A map of string values to pass to the external program as the query " +
+					"arguments. If not supplied, the program will receive an empty object as its input.",
 				Type:     schema.TypeMap,
 				Optional: true,
 				Elem: &schema.Schema{
@@ -38,8 +61,9 @@ func dataSource() *schema.Resource {
 			},
 
 			"result": {
-				Type:     schema.TypeMap,
-				Computed: true,
+				Description: "A map of string values returned from the external program.",
+				Type:        schema.TypeMap,
+				Computed:    true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
